cd56f02a35e4be5843e36b463bc7ce3a66f4db62
Lenovo <Lenovo@LAPTOP-0QKB0VA6>
2020-07-31 14:53:28 +0800

    ewq

---
+++ src/main/java/cn/tedu/sp06/Sp06Ribbon1Application.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp06Ribbon1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp06Ribbon1Application.class, args);
+	}
+
+}
